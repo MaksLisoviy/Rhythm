a8f932690dddd715b5dc33fb344448a1c7b26c7b
MaksLisoviy <128755236+MaksLisoviy@users.noreply.github.com>
2023-03-26 01:09:22 +0200

    Responsive guitar shop landing page

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+	const swiper = new Swiper('.swiper-reviews', {
+		// Optional parameters
+		autoHeight: true,
+		loop: true,
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+	});
+}
+
+
+
+const iconMenu = document.querySelector('.menu__icon')
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body')
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	})
+}
